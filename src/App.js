f59e90d6878f26438f0e5c11fb037b7127bb5c3d
manasa <lakshmimanasasai.t@gmail.com>
2019-12-17 10:27:45 +0530

    filter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import down from './down-arrow.svg';
 
 let data = [
   {
-    name: 'test', email: 'test@gmail.com', age: '1-1-1990',
-    experience: 1, position: 'Engineer', date: '1-1-2019', status: 'approved'
+    Name: 'test', Email: 'test@gmail.com', Age: '1-1-1990',
+    Experience: 1, Position: 'Engineer', Date: '1-1-2019', Status: 'approved'
   },
   {
-    name: 'test1', email: 'test1@gmail.com', age: '1-3-1990',
-    experience: 4, position: 'Manager', date: '1-3-2019', status: 'approved'
+    Name: 'test1', Email: 'test1@gmail.com', Age: '1-3-1990',
+    Experience: 4, Position: 'Manager', Date: '1-3-2019', Status: 'approved'
   },
   {
-    name: 'test2', email: 'test2@gmail.com', age: '1-2-1990',
-    experience: 2, position: 'Accountant', date: '1-2-2019', status: 'approved'
+    Name: 'test2', Email: 'test2@gmail.com', Age: '1-2-1990',
+    Experience: 2, Position: 'Accountant', Date: '1-2-2019', Status: 'approved'
   }
 ];
 
@@ -33,36 +32,64 @@ class App extends Component {
     this.setState({updatedList: data});
   }
 
-  handleSort = sortBy => {
-    let sortedArray = this.state.updatedList.sort((data1,data2) => (data1[sortBy] > data2[sortBy]) ? 1 : -1 );
+  handleSort = (sortBy, order) => {
+    let sortedArray = [];
+    if (order === 'a') {
+      sortedArray = this.state.updatedList.sort((data1,data2) => (data1[sortBy] > data2[sortBy]) ? 1 : -1 );
+    } else {
+      sortedArray = this.state.updatedList.sort((data1,data2) => (data1[sortBy] < data2[sortBy]) ? 1 : -1 );
+    }
     this.setState({updatedList: sortedArray});
   }
   render() {
     let displayList = this.state.updatedList.map(data => {
       return (
-        <tr key={data.email}>
-          <td>{data.name}</td><td>{data.email}</td><td>{data.age}</td>
-          <td>{data.experience}</td>
-          <td>{data.position}</td>
-          <td>{data.date}</td>
-          <td>{data.status}</td>
+        <tr key={data.Email}>
+          <td>{data.Name}</td><td>{data.Email}</td><td>{data.Age}</td>
+          <td>{data.Experience}</td>
+          <td>{data.Position}</td>
+          <td>{data.Date}</td>
+          <td>{data.Status}</td>
         </tr>
       );
    });
+   let sortHeaders = ['Experience', 'Position', 'Date'].map(header => {
+     return (
+       <th key={header}>
+         <svg onClick={e => this.handleSort(header, 'd')} className="arrow" x="0px" y="0px" viewBox="0 0 386.257 386.257">
+           <polygon points="0,96.879 193.129,289.379 386.257,96.879 "></polygon>
+         </svg>
+         {header}
+         <svg onClick={e => this.handleSort(header, 'a')} className="arrow up" x="0px" y="0px" viewBox="0 0 386.257 386.257">
+           <polygon points="0,96.879 193.129,289.379 386.257,96.879 "></polygon>
+         </svg>
+       </th>
+     );
+   });
     return (
       <div className="App">
+        <div style={{'display': 'flex', 'marginBottom': '15px',}}>
+          <span style={{'flexGrow': '1'}}>Filter By:</span>
+          <span style={{'flexGrow': '2'}}>Name:<br/><input onChange={this.handleOnChange} type="text"/></span>
+          <span style={{'flexGrow': '2'}}>Status:<br/>
+            <select name="status">
+              <option value="all">all</option>
+              <option value="approved">approved</option>
+              <option value="waiting">waiting</option>
+              <option value="reject">reject</option>
+            </select>
+          </span>
+          <span style={{'flexGrow': '2'}}>Position:<br/><input type="text"/></span>
+        </div>
         <table>
-          <tr>
-            <th>Name</th><th>Email</th><th>Age</th>
-            <th onClick={e => this.handleSort('experience')}>
-              {down}
-              Experience
-            </th>
-            <th onClick={e => this.handleSort('position')}>Position</th>
-            <th onClick={e => this.handleSort('date')}>Date</th>
-            <th>Status</th>
-          </tr>
-          {displayList}
+          <tbody>
+            <tr>
+              <th>Name</th><th>Email</th><th>Age</th>
+              {sortHeaders}
+              <th>Status</th>
+            </tr>
+            {displayList}
+          </tbody>
         </table>
       </div>
     );
